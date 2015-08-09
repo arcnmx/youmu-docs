243f905c0d4a788e12ea7423c3e5232f4beed083
ghost <ghost@konpaku.2hu>
2015-08-09 18:57:06 +0000

    db344931fce1707c3eadeb1274c15bdf115913cc #8

---
+++ serde/serde/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["bytes","Helper module to enable serializing bytes more efficiently"],["de","Generic deserialization framework."],["iter",""],["ser","Generic serialization framework."]]});
