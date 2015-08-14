f53e5f2a0dbc2c9129f313e60588e6cc66b8ffeb
ghost <ghost@konpaku.2hu>
2015-08-14 02:57:21 +0000

    b4a365f8ba9b02b3cc000e58be060c99d128c000

--- dotenv/implementors/core/clone/trait.Clone.js
+++ dotenv/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['dotenv'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='dotenv/enum.DotenvError.html' title='dotenv::DotenvError'>DotenvError</a>",];
+implementors['dotenv'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='dotenv/enum.DotenvError.html' title='dotenv::DotenvError'>DotenvError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- serde/serde/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"mod":[["bytes","Helper module to enable serializing bytes more efficiently"],["de","Generic deserialization framework."],["iter",""],["ser","Generic serialization framework."]]});
